d3d7e22ac07ebfa39c02b316bf78c4a26344a6eb
superbert <mildenberger.robert@googlemail.com>
2017-12-13 01:38:45 +0100

    added serviceviews

---
+++ content-dome/src/views/ServiceLeft.vue
@@ -0,0 +1,24 @@
+<template>
+  <transition :name="direction">
+  <div :class="[{mobile: viewport}, {left: !viewport}]" class="window">
+    <div class="middle backgroundBlack">
+      <img src="../assets/content_dome_immersive_media.png"/>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+import { langWatch, revUp } from '@/mixins'
+import { mapGetters } from 'vuex'
+
+export default {
+  mixins: [langWatch, revUp],
+  computed: {
+    ...mapGetters({
+      viewport: 'viewport'
+    }),
+    lang () { return this.$route.query.lang }
+  }
+}
+</script>
---
+++ content-dome/src/views/ServiceRight.vue
@@ -0,0 +1,31 @@
+<template>
+  <transition :name="direction">
+  <div :class="[{mobile: viewport}, {right: !viewport}]" class="window">
+    <div class="middle">
+      <div class="inner">
+        <br><br><p class="tMiddle text">{{ myText[lang] }}</p><br><br>
+        <p class="tMiddle text">{{ myText[lang+"1"] }}</p><br><br>
+        <p class="tMiddle text">{{ myText[lang+"2"] }}</p><br><br>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+import { langWatch, revUp } from '@/mixins'
+import { mapGetters } from 'vuex'
+
+export default {
+  mixins: [langWatch, revUp],
+  computed: {
+    ...mapGetters({
+      text: 'text',
+      viewport: 'viewport'
+    }),
+    myText () { return this.text.servicesLeft },
+    lang () { return this.$route.query.lang }
+  }
+}
+</script>
+
