24937b61e30361ca44d42f74807736b4b3e44048
superbert <mildenberger.robert@googlemail.com>
2018-01-06 21:14:08 +0100

    try fix apple bug last

--- content-dome/src/views/ContactLeft.vue
+++ content-dome/src/views/ContactLeft.vue
@@ -4,15 +4,15 @@
       <div :class="{mob: viewlandscape}" class="inner">
         <div :class="{mobl: viewlandscape}">
           <img v-show="!viewlandscape" src="../assets/content_dome_page.png"/>
-          <p class="tSmall text">{{ myText[lang][0] }}</p>
-          <p class="tSmall text">{{ myText[lang][1]}}</p><br>
-          <p class="tSmall text">{{ myText[lang][2]}}</p><br>
-          <p class="tSmall text">{{ myText[lang][3]}}</p><br>
-          <p class="tSmall text">{{ myText[lang][4]}}</p><br>
-          <p class="tSmall text">{{ myText[lang][5]}}</p><br>
+          <p class="tSpaceB tSmall text tab">{{ myText[lang][0] }}</p>
+          <p class="tSmall text">{{ myText[lang][1]}}</p><br/>
+          <p class="tSmall text">{{ myText[lang][2]}}</p><br/>
+          <p class="tSmall text">{{ myText[lang][3]}}</p><br/>
+          <p class="tSmall text">{{ myText[lang][4]}}</p><br/>
+          <p class="tSmall text">{{ myText[lang][5]}}</p><br/>
         </div>
         <div :class="{mobr: viewlandscape}">
-          <p class="tSmall text">{{ myText[lang][6]}}</p><br>
+          <p class="tSmall text">{{ myText[lang][6]}}</p><br/>
           <p class="tSmall disc text">{{ myText[lang][7]}}</p>
         </div>
       </div>
